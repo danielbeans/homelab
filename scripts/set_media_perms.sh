dab915cb57888aea02604f3be5501c260d673cb6
Daniel Williams <72041087+danielbeans@users.noreply.github.com>
2026-01-11 01:11:16 -0500

    Add script to set media permissions and ownership

---
+++ scripts/set_media_perms.sh
@@ -0,0 +1,22 @@
+# Set basic 755 and 644 folder and file permissions and user:group ownership
+
+#!/bin/bash
+
+# Check if both user and group arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <user> <group>"
+    exit 1
+fi
+
+USER=$1
+GROUP=$2
+
+echo "Applying permissions for $USER:$GROUP in the current directory..."
+
+sudo chown -R "$USER:$GROUP" .
+
+sudo find . -type d -exec chmod 755 {} +
+
+sudo find . -type f -exec chmod 664 {} +
+
+echo "Permissions updated successfully!"
